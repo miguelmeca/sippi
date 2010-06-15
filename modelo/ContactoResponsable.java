b3b421725b3bf08463d65ab5810766b0f6322de8
fermmanuel <fermmanuel@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-06-15 01:51:42 +0000

    Arreglando clases importadas

--- modelo/ContactoResponsable.java
+++ modelo/ContactoResponsable.java
@@ -11,25 +11,83 @@ package modelo;
 //
 //
 
-
-
-
 public class ContactoResponsable {
-	private String cargo;
+	private int id;
+        private String cargo;
 	private String nombre;
 	private String apellido;
 	private String email;
 	private String cuil;
 	private Telefono telefonos;
-	public void crear() {
-	
-	}
-	
-	public void mostrarContactoEmpresa() {
-	
-	}
-	
-	public void getContacto() {
-	
-	}
+
+    public ContactoResponsable() {
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCuil() {
+        return cuil;
+    }
+
+    public void setCuil(String cuil) {
+        this.cuil = cuil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Telefono getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(Telefono telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public void crear() {
+
+    }
+
+    public void mostrarContactoEmpresa() {
+
+    }
+
+    public void getContacto() {
+
+    }
 }
