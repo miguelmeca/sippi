57f7a00290362fe6d6c663db6173f4cbe1a08575
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-06-18 19:43:03 +0000

    Mas del mapeo a BD

--- trunk/modelo/Indumentaria.java
+++ trunk/modelo/Indumentaria.java
@@ -7,7 +7,7 @@ package modelo;
 //  @ Project : Proyecto2010_Requerimientos-iuga
 //  @ File Name : Indumentaria.java
 //  @ Date : 14/06/2010
-//  @ Author : 
+//  @ Author : Iuga
 //
 //
 
@@ -15,9 +15,49 @@ package modelo;
 
 
 public class Indumentaria {
-	private Object descripcion;
+	
+        private int id;
+        private String descripcion;
 	private Talle talle;
 	private TipoIndumentaria tipo;
+
+    public Indumentaria() {
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Talle getTalle() {
+        return talle;
+    }
+
+    public void setTalle(Talle talle) {
+        this.talle = talle;
+    }
+
+    public TipoIndumentaria getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoIndumentaria tipo) {
+        this.tipo = tipo;
+    }
+
+    
+
 	public void crear() {
 	
 	}
