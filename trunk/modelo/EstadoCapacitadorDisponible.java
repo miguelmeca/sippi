172a86c9610cd7b2dd9c9d688e01657814f70294
fmgs31 <fmgs31@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-06-30 14:24:36 +0000

    nuevoCapacitador

---
+++ trunk/modelo/EstadoCapacitadorDisponible.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class EstadoCapacitadorDisponible extends EstadoCapacitador
+{
+    @Override
+    public boolean estaDisponible()
+    {
+	return true;
+	
+    }
+
+}
