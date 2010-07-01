9dcb6be1d5f927a4cacf61df5c1d2b1a6fa73338
fmgs31 <fmgs31@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-07-01 03:40:40 +0000

    agragado el parametro boolean de exito

--- trunk/vista/interfaces/ICallBack.java
+++ trunk/vista/interfaces/ICallBack.java
@@ -14,8 +14,8 @@ public interface ICallBack {
      * Es una interface que define un metodo para actualizar una interfaz
      * una vez que se termino un caso de uso de extension o de inclusion.
      * La misma se activa cuando se finaliza el otro UC.
-     * @param int flag
+     * @param int flag, boolean exito
      */
-    public void actualizar(int flag);
+    public void actualizar(int flag, boolean exito);
 
 }
