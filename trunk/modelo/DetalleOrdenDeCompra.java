aaddf56adce148be5f66d2b96e9e86c900dcad59
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-08-28 22:26:37 +0000

    Ultima parte de los XML

---
+++ trunk/modelo/DetalleOrdenDeCompra.java
@@ -0,0 +1,55 @@
+package modelo;
+
+/**
+ * Descripción:
+ * @version 1.0
+ * @author Iuga
+ * @cambios
+ * @todo
+ */
+
+public class DetalleOrdenDeCompra {
+
+    private int id;
+    private int cantidad;
+    private double precio;
+    private Recurso recurso;
+
+    public DetalleOrdenDeCompra() {
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Recurso getRecurso() {
+        return recurso;
+    }
+
+    public void setRecurso(Recurso recurso) {
+        this.recurso = recurso;
+    }
+
+
+
+}
