c8b04ec000e70f464771c93d10740afd3fdb2868
fermmanuel@gmail.com <fermmanuel@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-09-02 23:30:34 +0000

    Avanzando con RegistrarRecepcionOC falta poco. Esto con un poco de arreglos se traslada a ConsultarOC

--- trunk/controlador/Compras/GestorRegistrarRecepcionOC.java
+++ trunk/controlador/Compras/GestorRegistrarRecepcionOC.java
@@ -6,9 +6,11 @@
 package controlador.Compras;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import modelo.DetalleOrdenDeCompra;
 import modelo.OrdenDeCompra;
 import modelo.Proveedor;
 import util.HibernateUtil;
@@ -27,10 +29,7 @@ public class GestorRegistrarRecepcionOC {
         this.pantalla = p;
     }
 
-    public ArrayList<NTupla> mostrarProveedoresConOrdenesPendientes(){
-        /**
-         * LA PAPA DE HIBERNATE
-         */
+    public ArrayList<NTupla> mostrarProveedoresConOCPendientes(){
         ArrayList<NTupla> provs = new ArrayList<NTupla>();
         NTupla nt = new NTupla();
         Proveedor pr = null;
@@ -48,7 +47,6 @@ public class GestorRegistrarRecepcionOC {
         } catch (Exception ex) {
             Logger.getLogger(GestorRegistrarRecepcionOC.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return provs;
     }
 
@@ -70,5 +68,69 @@ public class GestorRegistrarRecepcionOC {
             Logger.getLogger(GestorRegistrarRecepcionOC.class.getName()).log(Level.SEVERE, null, ex);
         }
         return ordenes;
+    }
+
+    public ArrayList<NTupla> buscarOCPorNro(int nro){
+        OrdenDeCompra oc=null;
+        ArrayList<NTupla> ordenes = new ArrayList<NTupla>();
+        NTupla nt = null;
+        try {
+            Iterator it = HibernateUtil.getSession().createQuery("from OrdenDeCompra oc where oc.id=:idOC").setParameter("idOC",nro).iterate();
+            while(it.hasNext()){
+                oc = (OrdenDeCompra)it.next();
+                nt = new NTupla();
+                nt.setId(oc.getId());
+                nt.setNombre(oc.getProveedor().getRazonSocial());
+                nt.setData(oc.getFechaDePedido());
+                ordenes.add(nt);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(GestorRegistrarRecepcionOC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ordenes;
+    }
+
+        public ArrayList<NTupla> buscarOCPorFechaEmision(Date fecha){
+        OrdenDeCompra oc=null;
+        ArrayList<NTupla> ordenes = new ArrayList<NTupla>();
+        NTupla nt = null;
+        try {
+            Iterator it = HibernateUtil.getSession().createQuery("from OrdenDeCompra oc where oc.fechaDePedido=:fecha").setParameter("fecha",fecha).iterate();
+            while(it.hasNext()){
+                oc = (OrdenDeCompra)it.next();
+                nt = new NTupla();
+                nt.setId(oc.getId());
+                nt.setNombre(oc.getProveedor().getRazonSocial());
+                nt.setData(oc.getFechaDePedido());
+                ordenes.add(nt);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(GestorRegistrarRecepcionOC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ordenes;
+    }
+
+    public ArrayList<NTupla> mostrarDetalleOC(int idOC){
+        DetalleOrdenDeCompra doc=null;
+        ArrayList<NTupla> detalles = new ArrayList<NTupla>();
+        NTupla nt = null;
+        String[] datos = new String[3];
+        try {
+            Iterator it = HibernateUtil.getSession().createQuery("from DetalleOrdenDeCompra doc WHERE (FROM OrdenDeCompra oc WHERE oc=:idOC AND oc.detalle=doc.id)").setParameter("idOC",idOC).iterate();
+            while(it.hasNext()){
+                doc = (DetalleOrdenDeCompra)it.next();
+                nt = new NTupla();
+                nt.setId(doc.getId());
+                nt.setNombre(doc.getRecurso().getNombre());
+                datos[0] = new String(String.valueOf(doc.getCantidad()));
+                datos[1] = new String(String.valueOf(doc.getRecurso().getDescipcion()));
+                datos[2] = new String(String.valueOf(doc.getPrecio()));
+                nt.setData(datos);
+                detalles.add(nt);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(GestorRegistrarRecepcionOC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return detalles;
     }
 }
