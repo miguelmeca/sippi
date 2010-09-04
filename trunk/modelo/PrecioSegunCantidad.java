2115be0fcb7e216a311c3e99f4e9165c27a763e9
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-09-04 19:06:51 +0000

    Agregue fechaVigencia en PrecioSegunCantidad y en el XML !!

--- trunk/modelo/PrecioSegunCantidad.java
+++ trunk/modelo/PrecioSegunCantidad.java
@@ -16,6 +16,7 @@ public class PrecioSegunCantidad
     private Date fecha;
     private int cantidad;
     private double precio;
+    private Date fechaVigencia;
 
     public int getCantidad() {
         return cantidad;
@@ -49,5 +50,14 @@ public class PrecioSegunCantidad
         this.precio = precio;
     }
 
+    public Date getFechaVigencia() {
+        return fechaVigencia;
+    }
+
+    public void setFechaVigencia(Date fechaVigencia) {
+        this.fechaVigencia = fechaVigencia;
+    }
+
+    
 
 }
