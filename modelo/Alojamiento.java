3fd387e59dcdfe4c0f792ec245ba0a9e15830bcb
fmgs31 <fmgs31@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2010-10-17 04:38:54 +0000

    nuevas clases del modelo y arreglo d Tarea

--- modelo/Alojamiento.java
+++ modelo/Alojamiento.java
@@ -2,18 +2,52 @@ package modelo;
 
 /**
  * Descripción:
- * @version 1.0
- * @author Iuga
- * @cambios
- * @todo
- */
+ * @version 2.0
+ * @author Fran
+ **/
 
-public class Alojamiento extends Recurso{
+public class Alojamiento
+{
+    private int id;
+    private Proveedor empresa;
+    private int cantidad;
+    private double precioUnitario;
 
-    @Override
-    public String toString()
+
+    public Alojamiento()
     {
-        return "Alojamiento";
     }
 
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Proveedor getEmpresa() {
+        return this.empresa;
+    }
+
+    public void setEmpresa(Proveedor empresa) {
+        this.empresa = empresa;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cant) {
+        this.cantidad = cant;
+    }
+    public double getPrecio() {
+        return this.precioUnitario;
+    }
+
+    public void setPrecio(double precio) {
+        this.precioUnitario = precio;
+    }
+
+    
 }
--- modelo/TransporteDePasajeros.java
+++ modelo/TransporteDePasajeros.java
@@ -6,14 +6,67 @@ package modelo;
 //
 //  @ Project : Proyecto2010_Requerimientos-iuga
 //  @ File Name : TransporteDePasajeros.java
-//  @ Date : 08/10/2010
-//  @ Author : 
+//  @ Date : 16/10/2010
+//  @ Author : Fran a mano...
 //
 //
 
-public class TransporteDePasajeros extends Recurso {
-    public String toString()
+public class TransporteDePasajeros
+{
+    private int id;
+    private Proveedor empresa;
+    private int cantidad;
+    private double precio;
+    private Localidad origen;
+    private Localidad destino;
+
+    public TransporteDePasajeros()
     {
-        return "TransporteDePasajeros";
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Proveedor getEmpresa() {
+        return this.empresa;
+    }
+
+    public void setEmpresa(Proveedor empresa) {
+        this.empresa = empresa;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cant) {
+        this.cantidad = cant;
+    }
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public Localidad getOrigen() {
+        return this.origen;
+    }
+
+    public void setOrigen(Localidad origen) {
+        this.origen = origen;
+    }
+
+    public Localidad getDestino() {
+        return this.destino;
+    }
+
+    public void setDestino(Localidad destino) {
+        this.destino = destino;
     }
 }
