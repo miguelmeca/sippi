d667093e0cae4140ab7158bd86bb1c084e7f8ca2
fmgs31 <fmgs31@gmail.com>
2011-07-10 20:35:36 +0000

    Grafico Gantt

--- vista/prototipos/editarCotizacion_ManoDeObra_OpcionB_parte1.java
+++ vista/prototipos/editarCotizacion_ManoDeObra_OpcionB_parte1.java
@@ -11,16 +11,172 @@
 
 package vista.prototipos;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.IntervalCategoryDataset;
+import org.jfree.data.gantt.Task;
+import org.jfree.data.gantt.TaskSeries;
+import org.jfree.data.gantt.TaskSeriesCollection;
+import org.jfree.data.time.SimpleTimePeriod;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
 /**
  *
  * @author Fran
  */
 public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPanel {
 
-    /** Creates new form pantallaManoDeObra */
-    public editarCotizacion_ManoDeObra_OpcionB_parte1() {
+    public editarCotizacion_ManoDeObra_OpcionB_parte1()
+    {
         initComponents();
+        initGraphs();
     }
+    private void initGraphs()
+    {
+         final IntervalCategoryDataset dataset = createDataset();
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        //chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        chartPanel.setSize(panelGantt.getSize());
+        panelGantt.add( chartPanel );
+        panelGantt.getParent().validate();
+    }
+
+     /**
+     * Creates a sample dataset for a Gantt chart.
+     *
+     * @return The dataset.
+     */
+    public static IntervalCategoryDataset createDataset() {
+
+        final TaskSeries s1 = new TaskSeries("Scheduled");
+        s1.add(new Task("Write Proposal",
+               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
+                                    date(5, Calendar.APRIL, 2001))));
+        s1.add(new Task("Obtain Approval",
+               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
+                                    date(9, Calendar.APRIL, 2001))));
+        s1.add(new Task("Requirements Analysis",
+               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
+                                    date(5, Calendar.MAY, 2001))));
+        s1.add(new Task("Design Phase",
+               new SimpleTimePeriod(date(6, Calendar.MAY, 2001),
+                                    date(30, Calendar.MAY, 2001))));
+        s1.add(new Task("Design Signoff",
+               new SimpleTimePeriod(date(2, Calendar.JUNE, 2001),
+                                    date(2, Calendar.JUNE, 2001))));
+        s1.add(new Task("Alpha Implementation",
+               new SimpleTimePeriod(date(3, Calendar.JUNE, 2001),
+                                    date(31, Calendar.JULY, 2001))));
+        s1.add(new Task("Design Review",
+               new SimpleTimePeriod(date(1, Calendar.AUGUST, 2001),
+                                    date(8, Calendar.AUGUST, 2001))));
+        s1.add(new Task("Revised Design Signoff",
+               new SimpleTimePeriod(date(10, Calendar.AUGUST, 2001),
+                                    date(10, Calendar.AUGUST, 2001))));
+        s1.add(new Task("Beta Implementation",
+               new SimpleTimePeriod(date(12, Calendar.AUGUST, 2001),
+                                    date(12, Calendar.SEPTEMBER, 2001))));
+        s1.add(new Task("Testing",
+               new SimpleTimePeriod(date(13, Calendar.SEPTEMBER, 2001),
+                                    date(31, Calendar.OCTOBER, 2001))));
+        s1.add(new Task("Final Implementation",
+               new SimpleTimePeriod(date(1, Calendar.NOVEMBER, 2001),
+                                    date(15, Calendar.NOVEMBER, 2001))));
+        s1.add(new Task("Signoff",
+               new SimpleTimePeriod(date(28, Calendar.NOVEMBER, 2001),
+                                    date(30, Calendar.NOVEMBER, 2001))));
+
+        final TaskSeries s2 = new TaskSeries("Actual");
+        s2.add(new Task("Write Proposal",
+               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
+                                    date(5, Calendar.APRIL, 2001))));
+        s2.add(new Task("Obtain Approval",
+               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
+                                    date(9, Calendar.APRIL, 2001))));
+        s2.add(new Task("Requirements Analysis",
+               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
+                                    date(15, Calendar.MAY, 2001))));
+        s2.add(new Task("Design Phase",
+               new SimpleTimePeriod(date(15, Calendar.MAY, 2001),
+                                    date(17, Calendar.JUNE, 2001))));
+        s2.add(new Task("Design Signoff",
+               new SimpleTimePeriod(date(30, Calendar.JUNE, 2001),
+                                    date(30, Calendar.JUNE, 2001))));
+        s2.add(new Task("Alpha Implementation",
+               new SimpleTimePeriod(date(1, Calendar.JULY, 2001),
+                                    date(12, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Design Review",
+               new SimpleTimePeriod(date(12, Calendar.SEPTEMBER, 2001),
+                                    date(22, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Revised Design Signoff",
+               new SimpleTimePeriod(date(25, Calendar.SEPTEMBER, 2001),
+                                    date(27, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Beta Implementation",
+               new SimpleTimePeriod(date(27, Calendar.SEPTEMBER, 2001),
+                                    date(30, Calendar.OCTOBER, 2001))));
+        s2.add(new Task("Testing",
+               new SimpleTimePeriod(date(31, Calendar.OCTOBER, 2001),
+                                    date(17, Calendar.NOVEMBER, 2001))));
+        s2.add(new Task("Final Implementation",
+               new SimpleTimePeriod(date(18, Calendar.NOVEMBER, 2001),
+                                    date(5, Calendar.DECEMBER, 2001))));
+        s2.add(new Task("Signoff",
+               new SimpleTimePeriod(date(10, Calendar.DECEMBER, 2001),
+                                    date(11, Calendar.DECEMBER, 2001))));
+
+        final TaskSeriesCollection collection = new TaskSeriesCollection();
+        collection.add(s1);
+        collection.add(s2);
+
+        return collection;
+    }
+
+    /**
+     * Utility method for creating <code>Date</code> objects.
+     *
+     * @param day  the date.
+     * @param month  the month.
+     * @param year  the year.
+     *
+     * @return a date.
+     */
+    private static Date date(final int day, final int month, final int year) {
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        final Date result = calendar.getTime();
+        return result;
+
+    }
+        
+    /**
+     * Creates a chart.
+     * 
+     * @param dataset  the dataset.
+     * 
+     * @return The chart.
+     */
+    private JFreeChart createChart(final IntervalCategoryDataset dataset) {
+        final JFreeChart chart = ChartFactory.createGanttChart(
+            "Gantt Chart Demo",  // chart title
+            "Task",              // domain axis label
+            "Date",              // range axis label
+            dataset,             // data
+            true,                // include legend
+            true,                // tooltips
+            false                // urls
+        );    
+//        chart.getCategoryPlot().getDomainAxis().setMaxCategoryLabelWidthRatio(10.0f);
+        return chart;    
+    }
+
 
     /** This method is called from within the constructor to
      * initialize the form.
@@ -41,7 +197,7 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
         jPanel4 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jPanel3 = new javax.swing.JPanel();
+        panelGantt = new javax.swing.JPanel();
         jTextField1 = new javax.swing.JTextField();
 
         jLabel9.setText("Plano");
@@ -83,16 +239,16 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
         });
         jScrollPane2.setViewportView(jTable1);
 
-        jPanel3.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        panelGantt.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout panelGanttLayout = new javax.swing.GroupLayout(panelGantt);
+        panelGantt.setLayout(panelGanttLayout);
+        panelGanttLayout.setHorizontalGroup(
+            panelGanttLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 384, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        panelGanttLayout.setVerticalGroup(
+            panelGanttLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 131, Short.MAX_VALUE)
         );
 
@@ -103,14 +259,14 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelGantt, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 390, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelGantt, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -163,13 +319,13 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
+    private javax.swing.JPanel panelGantt;
     // End of variables declaration//GEN-END:variables
 
 }
