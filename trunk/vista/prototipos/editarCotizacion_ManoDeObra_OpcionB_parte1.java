f18d7a6714f09b35810f84b6b0c2469606631ed5
fmgs31@gmail.com <fmgs31@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2011-07-11 01:02:55 +0000

    Gant Listo

--- trunk/vista/prototipos/editarCotizacion_ManoDeObra_OpcionB_parte1.java
+++ trunk/vista/prototipos/editarCotizacion_ManoDeObra_OpcionB_parte1.java
@@ -43,7 +43,9 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
         final JFreeChart chart = createChart(dataset);
         final ChartPanel chartPanel = new ChartPanel(chart);
         //chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        chartPanel.setSize(panelGantt.getSize());
+        chartPanel.setSize(panelGantt.getWidth(),panelGantt.getHeight());
+        //chartPanel.setSize(panelGantt.getHeight(), panelGantt.getWidth());
+        chartPanel.setSize(390,137);
         panelGantt.add( chartPanel );
         panelGantt.getParent().validate();
     }
@@ -55,85 +57,28 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
      */
     public static IntervalCategoryDataset createDataset() {
 
-        final TaskSeries s1 = new TaskSeries("Scheduled");
-        s1.add(new Task("Write Proposal",
+        final TaskSeries s1 = new TaskSeries("Tareas");
+        s1.add(new Task("Montaje Parte G4",
                new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
                                     date(5, Calendar.APRIL, 2001))));
-        s1.add(new Task("Obtain Approval",
+        s1.add(new Task("Traslado",
                new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
                                     date(9, Calendar.APRIL, 2001))));
-        s1.add(new Task("Requirements Analysis",
+        s1.add(new Task("Montaje Parte G5",
                new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
                                     date(5, Calendar.MAY, 2001))));
-        s1.add(new Task("Design Phase",
+        s1.add(new Task("Ensamblaje en fabrica G4",
                new SimpleTimePeriod(date(6, Calendar.MAY, 2001),
                                     date(30, Calendar.MAY, 2001))));
-        s1.add(new Task("Design Signoff",
+        s1.add(new Task("Puesta a punto",
                new SimpleTimePeriod(date(2, Calendar.JUNE, 2001),
                                     date(2, Calendar.JUNE, 2001))));
-        s1.add(new Task("Alpha Implementation",
-               new SimpleTimePeriod(date(3, Calendar.JUNE, 2001),
-                                    date(31, Calendar.JULY, 2001))));
-        s1.add(new Task("Design Review",
-               new SimpleTimePeriod(date(1, Calendar.AUGUST, 2001),
-                                    date(8, Calendar.AUGUST, 2001))));
-        s1.add(new Task("Revised Design Signoff",
-               new SimpleTimePeriod(date(10, Calendar.AUGUST, 2001),
-                                    date(10, Calendar.AUGUST, 2001))));
-        s1.add(new Task("Beta Implementation",
-               new SimpleTimePeriod(date(12, Calendar.AUGUST, 2001),
-                                    date(12, Calendar.SEPTEMBER, 2001))));
-        s1.add(new Task("Testing",
-               new SimpleTimePeriod(date(13, Calendar.SEPTEMBER, 2001),
-                                    date(31, Calendar.OCTOBER, 2001))));
-        s1.add(new Task("Final Implementation",
-               new SimpleTimePeriod(date(1, Calendar.NOVEMBER, 2001),
-                                    date(15, Calendar.NOVEMBER, 2001))));
-        s1.add(new Task("Signoff",
-               new SimpleTimePeriod(date(28, Calendar.NOVEMBER, 2001),
-                                    date(30, Calendar.NOVEMBER, 2001))));
+        
 
-        final TaskSeries s2 = new TaskSeries("Actual");
-        s2.add(new Task("Write Proposal",
-               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
-                                    date(5, Calendar.APRIL, 2001))));
-        s2.add(new Task("Obtain Approval",
-               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
-                                    date(9, Calendar.APRIL, 2001))));
-        s2.add(new Task("Requirements Analysis",
-               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
-                                    date(15, Calendar.MAY, 2001))));
-        s2.add(new Task("Design Phase",
-               new SimpleTimePeriod(date(15, Calendar.MAY, 2001),
-                                    date(17, Calendar.JUNE, 2001))));
-        s2.add(new Task("Design Signoff",
-               new SimpleTimePeriod(date(30, Calendar.JUNE, 2001),
-                                    date(30, Calendar.JUNE, 2001))));
-        s2.add(new Task("Alpha Implementation",
-               new SimpleTimePeriod(date(1, Calendar.JULY, 2001),
-                                    date(12, Calendar.SEPTEMBER, 2001))));
-        s2.add(new Task("Design Review",
-               new SimpleTimePeriod(date(12, Calendar.SEPTEMBER, 2001),
-                                    date(22, Calendar.SEPTEMBER, 2001))));
-        s2.add(new Task("Revised Design Signoff",
-               new SimpleTimePeriod(date(25, Calendar.SEPTEMBER, 2001),
-                                    date(27, Calendar.SEPTEMBER, 2001))));
-        s2.add(new Task("Beta Implementation",
-               new SimpleTimePeriod(date(27, Calendar.SEPTEMBER, 2001),
-                                    date(30, Calendar.OCTOBER, 2001))));
-        s2.add(new Task("Testing",
-               new SimpleTimePeriod(date(31, Calendar.OCTOBER, 2001),
-                                    date(17, Calendar.NOVEMBER, 2001))));
-        s2.add(new Task("Final Implementation",
-               new SimpleTimePeriod(date(18, Calendar.NOVEMBER, 2001),
-                                    date(5, Calendar.DECEMBER, 2001))));
-        s2.add(new Task("Signoff",
-               new SimpleTimePeriod(date(10, Calendar.DECEMBER, 2001),
-                                    date(11, Calendar.DECEMBER, 2001))));
+        
 
         final TaskSeriesCollection collection = new TaskSeriesCollection();
         collection.add(s1);
-        collection.add(s2);
 
         return collection;
     }
@@ -165,9 +110,9 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
      */
     private JFreeChart createChart(final IntervalCategoryDataset dataset) {
         final JFreeChart chart = ChartFactory.createGanttChart(
-            "Gantt Chart Demo",  // chart title
-            "Task",              // domain axis label
-            "Date",              // range axis label
+            "",  // chart title
+            "",              // domain axis label
+            "",              // range axis label
             dataset,             // data
             true,                // include legend
             true,                // tooltips
@@ -240,6 +185,7 @@ public class editarCotizacion_ManoDeObra_OpcionB_parte1 extends javax.swing.JPan
         jScrollPane2.setViewportView(jTable1);
 
         panelGantt.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        panelGantt.setAutoscrolls(true);
 
         javax.swing.GroupLayout panelGanttLayout = new javax.swing.GroupLayout(panelGantt);
         panelGantt.setLayout(panelGanttLayout);
