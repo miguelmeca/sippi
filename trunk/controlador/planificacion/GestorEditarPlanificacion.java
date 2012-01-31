2cad762e8be2b43e82b2283acde88573729df473
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-01-31 23:28:54 +0000

    Linea de Tiempo ... sigo con los eventos, me voy a poner a full cuando este terminado el MODP + Hibernate con los ejemplos cargados

--- trunk/controlador/planificacion/GestorEditarPlanificacion.java
+++ trunk/controlador/planificacion/GestorEditarPlanificacion.java
@@ -8,6 +8,9 @@ package controlador.planificacion;
 import controlador.GestorAbstracto;
 import java.util.ArrayList;
 import java.util.List;
+import modelo.PedidoObra;
+import org.hibernate.Session;
+import util.HibernateUtil;
 import util.NTupla;
 import vista.planificacion.EditarPlanificacion;
 
@@ -19,56 +22,66 @@ public class GestorEditarPlanificacion extends GestorAbstracto {
 
     private EditarPlanificacion _pantalla;
     private GestorArbolDeRecursos _gestorArbolRecursos;
+    private Session sesion;
+    private PedidoObra pedidoDeObra;
 
     public GestorEditarPlanificacion(EditarPlanificacion _pantalla) {
         this._pantalla = _pantalla;
         this._gestorArbolRecursos = new GestorArbolDeRecursos();
     }
 
-    public List getListaSubObras() {
-        //TODO: Cambiar por la referencia 
-        //int idPlanificacion = 1;
-        //return _gestorArbolRecursos.getListaSubObras(idPlanificacion);
-
+    public List getListaSubObras(int idPedidoDeObra) {
+          
         ArrayList<NTupla> lista = new ArrayList<NTupla>();
+        
+        try
+        {
+            sesion= HibernateUtil.getSession();
+            pedidoDeObra = (PedidoObra) sesion.load(PedidoObra.class,idPedidoDeObra);
+            
+//            pedidoDeObra.ge
+//            NTupla nt1 = new NTupla(1);
+//            nt1.setNombre("TestSubTarea1:"+(Math.random()*100));    
+            
+        }catch(Exception e)
+        {
+            System.err.println("No se pudo cargar las subobras del pedido");
+        }
+        
+        //return lista;
+        return getListaMock("Sub Obra");
 
-            NTupla nt1 = new NTupla(1);
-            nt1.setNombre("TestSubTarea1:"+(Math.random()*100));
-
-            NTupla nt2 = new NTupla(2);
-            nt2.setNombre("TestSubTarea2:"+(Math.random()*100));
-            
-            NTupla nt3 = new NTupla(3);
-            nt3.setNombre("TestSubTarea3:"+(Math.random()*100));
-            
-            lista.add(nt1);
-            lista.add(nt2);
-            lista.add(nt3);
-            
-        return lista;
     }
     
     public List getListaTareasXSubObra(int idSubObra) {
-        //TODO: Cambiar por la referencia 
-        //int idPlanificacion = 1;
-        //return _gestorArbolRecursos.getListaSubObras(idPlanificacion);
-
+        
+        return getListaMock("Tarea");
+        
+    }    
+    
+    public List getListaMock(String tipo) {
+   
         ArrayList<NTupla> lista = new ArrayList<NTupla>();
 
             NTupla nt1 = new NTupla(1);
-            nt1.setNombre("Tarea 1:"+(Math.random()*100));
+            nt1.setNombre(tipo+" 1:"+(Math.random()*100));
 
             NTupla nt2 = new NTupla(2);
-            nt2.setNombre("Tarea 2:"+(Math.random()*100));
+            nt2.setNombre(tipo+" 2:"+(Math.random()*100));
             
             NTupla nt3 = new NTupla(3);
-            nt3.setNombre("Tarea 3:"+(Math.random()*100));
+            nt3.setNombre(tipo+" 3:"+(Math.random()*100));
+            
+            NTupla nt4 = new NTupla(4);
+            nt4.setNombre(tipo+" 4:"+(Math.random()*100));            
             
             lista.add(nt1);
             lista.add(nt2);
             lista.add(nt3);
+            lista.add(nt4);
             
-        return lista;
-    }    
+        return lista;        
+        
+    }
     
 }
