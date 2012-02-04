bbbc527925fd0d9513beeb9c04a046689069271c
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-02-04 23:29:52 +0000

    Carga de los datos de - planificacion - Cotizacion - Pedido de Obra {On going}

--- trunk/controlador/planificacion/GestorEditarPlanificacion.java
+++ trunk/controlador/planificacion/GestorEditarPlanificacion.java
@@ -9,7 +9,10 @@ import controlador.GestorAbstracto;
 import java.util.ArrayList;
 import java.util.List;
 import modelo.PedidoObra;
+import modelo.PlanificacionXXX;
+import modelo.SubObraModificada;
 import org.hibernate.Session;
+import util.FechaUtil;
 import util.HibernateUtil;
 import util.NTupla;
 import vista.planificacion.EditarPlanificacion;
@@ -24,32 +27,52 @@ public class GestorEditarPlanificacion extends GestorAbstracto {
     private GestorArbolDeRecursos _gestorArbolRecursos;
     private Session sesion;
     private PedidoObra pedidoDeObra;
+    private PlanificacionXXX planificacion;
 
-    public GestorEditarPlanificacion(EditarPlanificacion _pantalla) {
+    public GestorEditarPlanificacion(EditarPlanificacion _pantalla, int idPedidoDeObra) {
         this._pantalla = _pantalla;
         this._gestorArbolRecursos = new GestorArbolDeRecursos();
+        
+        try
+        {
+            sesion= HibernateUtil.getSession();
+            
+            this.pedidoDeObra = (PedidoObra) sesion.load(PedidoObra.class,idPedidoDeObra);
+            this.planificacion = pedidoDeObra.getPlanificacion();              
+            
+        }catch(Exception e)
+        {
+            mostrarMensajeError("No se pudo cargar el Pedido ni la planificación asociada");
+        }        
+        
     }
 
-    public List getListaSubObras(int idPedidoDeObra) {
+    public List getListaSubObras() {
           
         ArrayList<NTupla> lista = new ArrayList<NTupla>();
         
         try
         {
-            sesion= HibernateUtil.getSession();
-            pedidoDeObra = (PedidoObra) sesion.load(PedidoObra.class,idPedidoDeObra);
             
-//            pedidoDeObra.ge
-//            NTupla nt1 = new NTupla(1);
-//            nt1.setNombre("TestSubTarea1:"+(Math.random()*100));    
+             if(this.planificacion!=null && this.planificacion.getId()!=0)
+             {
+                 ArrayList<SubObraModificada> listaSO = (ArrayList) this.planificacion.getCotizacion().getSubObra();
+                 for (int i = 0; i < listaSO.size(); i++) 
+                 {
+                     SubObraModificada som = listaSO.get(i);
+                     NTupla nt1 = new NTupla(som.getId());
+                     nt1.setNombre("xxx"+1+"xxx");
+                 }
+             }
+               
             
         }catch(Exception e)
         {
-            System.err.println("No se pudo cargar las subobras del pedido");
+            mostrarMensajeError("No se pudo cargar las subobras del pedido");
         }
         
         //return lista;
-        return getListaMock("Sub Obra");
+        return lista;
 
     }
     
@@ -82,6 +105,45 @@ public class GestorEditarPlanificacion extends GestorAbstracto {
             
         return lista;        
         
+    }
+
+    public void mostrarDatosGenerales(int idPedidoDeObra) {
+        
+        try
+        {
+            sesion= HibernateUtil.getSession();
+            pedidoDeObra = (PedidoObra) sesion.load(PedidoObra.class,idPedidoDeObra);
+            
+            _pantalla.setLblObraNombre(pedidoDeObra.getNombre().substring(0,60)+"..."); 
+            _pantalla.setLblObraFechaFin(FechaUtil.getFecha(pedidoDeObra.getFechaFin()));
+            _pantalla.setLblObraFechaInicio(FechaUtil.getFecha(pedidoDeObra.getFechaInicio()));
+            _pantalla.setLblObraLugar(pedidoDeObra.getPlanta().getDomicilio().toString());
+            _pantalla.setLblObraPlanta(pedidoDeObra.getPlanta().getRazonSocial());
+
+            if(this.planificacion!=null && this.planificacion.getCotizacion()!=null)
+            {
+                _pantalla.setNroPlanificacion(String.valueOf(this.planificacion.getNumeroPlanificacion()));
+                _pantalla.setFechaInicioPlanif(this.planificacion.getFechaInicio());
+                _pantalla.setFechaFinPlanif(this.planificacion.getFechaFin());
+            }            
+            
+            if(this.planificacion!=null && this.planificacion.getCotizacion()!=null && this.planificacion.getCotizacion().getCotizacionOriginal()!=null)
+            {
+            _pantalla.setTxtNroCotizacion(this.planificacion.getCotizacion().getCotizacionOriginal().getNroCotizacion());
+            _pantalla.setCotizacionMontoTotal(String.valueOf(this.planificacion.getCotizacion().getCotizacionOriginal().CalcularTotal()));
+            }
+            
+            
+        }catch(Exception e)
+        {
+            mostrarMensajeError("No se pudo cargar los datos generales de la cotizacion");
+        }  
+        
+    }
+    
+    private void mostrarMensajeError(String msg)
+    {
+        System.err.println("[ERROR] "+msg);
     }
     
 }
