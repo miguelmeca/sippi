aafdc9ee3db0a9043cf03d0447d9ac19325c647d
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-03-11 21:17:35 +0000

    ABM GENERICOS
    - Definiciones de la interfaz y de los patrones a usar

---
+++ test/TestABM.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import vista.gen.PantallaABMGenerica;
+
+/**
+ *
+ * @author Administrador
+ */
+public class TestABM extends PantallaABMGenerica {
+
+    public TestABM(int comportamiento) {
+        super(comportamiento);
+    }
+    
+}
