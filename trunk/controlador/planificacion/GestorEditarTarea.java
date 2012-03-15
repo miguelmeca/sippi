956e7061686d587620726f565cfa6eff6824e0c8
fermmanuel@gmail.com <fermmanuel@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-03-15 11:34:52 +0000

    Ventana Editar Tarea y Ventana Datos Generales funcionando con el Gantt. No borré la clase "GanttEventMouse" pero está duplicada en la clase EditarPlanificacion.java

---
+++ trunk/controlador/planificacion/GestorEditarTarea.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controlador.planificacion;
+
+
+import modelo.PlanificacionXXX;
+import modelo.TareaPlanificacion;
+import vista.planificacion.PantallaEditarTarea;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class GestorEditarTarea implements IGestorPlanificacion{
+    private PantallaEditarTarea pantalla;
+
+    private TareaPlanificacion tarea;
+
+    // GESTOR PADRE
+    private GestorEditarPlanificacion gestorPadre;
+
+    // LISTA DE GESTORES
+    private GestorPlanificacionDatosGenerales gestorDatosGenerales;
+    private GestorPlanificacionSubTareas gestorSubTareas;
+    private GestorPlanificacionAsigEmpleados gestorAsigEmpleados;
+    private GestorPlanificacionHerramientas gestorHerramientas;
+    private GestorPlanificacionMateriales gestorMateriales;
+    private GestorPlanificacionAlquileresCompras gestorAlquileresCompras;
+    
+
+    public GestorEditarTarea(GestorEditarPlanificacion gestor)
+    {
+        this.gestorPadre = gestor;
+    }
+
+    public void setPantalla(PantallaEditarTarea pantalla) {
+        this.pantalla = pantalla;
+    }
+
+    public void seleccionarTarea(int idTarea)
+    {
+        for (int i = 0; i < getPlanificacion().getTareas().size(); i++)
+        {
+            TareaPlanificacion tp = (TareaPlanificacion)getPlanificacion().getTareas().get(i);
+            if(tp.getId()==idTarea)
+            {
+                this.tarea = tp;
+                break;
+            }
+        }
+    }
+
+    public GestorPlanificacionDatosGenerales getGestorDatosGenerales()
+    {
+        if(this.gestorDatosGenerales==null)
+        {
+            this.gestorDatosGenerales = new GestorPlanificacionDatosGenerales(this);
+        }
+        return gestorDatosGenerales;
+    }
+
+    public GestorPlanificacionSubTareas getGestorSubTareas()
+    {
+        if(this.gestorSubTareas==null)
+        {
+            this.gestorSubTareas = new GestorPlanificacionSubTareas(this);
+        }
+        return gestorSubTareas;
+    }
+
+    public GestorPlanificacionAsigEmpleados getGestorAsigEmpleados()
+    {
+        if(this.gestorAsigEmpleados==null)
+        {
+            this.gestorAsigEmpleados = new GestorPlanificacionAsigEmpleados(this);
+        }
+        return gestorAsigEmpleados;
+    }
+
+    public GestorPlanificacionHerramientas getGestorHerramientas()
+    {
+        if(this.gestorHerramientas==null)
+        {
+            this.gestorHerramientas = new GestorPlanificacionHerramientas(this);
+        }
+        return gestorHerramientas;
+    }
+
+    public GestorPlanificacionMateriales getGestorMateriales()
+    {
+        if(this.gestorMateriales==null)
+        {
+            this.gestorMateriales = new GestorPlanificacionMateriales(this);
+        }
+        return gestorMateriales;
+    }
+
+    public GestorPlanificacionAlquileresCompras getGestorAlquileresCompras()
+    {
+        if(this.gestorAlquileresCompras==null)
+        {
+            this.gestorAlquileresCompras = new GestorPlanificacionAlquileresCompras(this);
+        }
+        return gestorAlquileresCompras;
+    }
+
+    @Override
+    public void refrescarPantallas() {
+        gestorPadre.refrescarPantallas();
+        pantalla.actualizar();
+    }
+
+    @Override
+    public PlanificacionXXX getPlanificacion() {
+        return this.gestorPadre.getPlanificacion();
+    }
+
+    @Override
+    public TareaPlanificacion getTareaActual() {
+        return this.tarea;
+    }
+}
---
+++ trunk/controlador/planificacion/GestorPlanificacionAsigEmpleados.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controlador.planificacion;
+
+import modelo.PlanificacionXXX;
+import modelo.TareaPlanificacion;
+
+/**
+ *
+ * @author Emmanuel
+ */
+class GestorPlanificacionAsigEmpleados implements IGestorPlanificacion{
+
+    GestorPlanificacionAsigEmpleados(GestorEditarTarea aThis) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public PlanificacionXXX getPlanificacion() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public TareaPlanificacion getTareaActual() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void refrescarPantallas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
