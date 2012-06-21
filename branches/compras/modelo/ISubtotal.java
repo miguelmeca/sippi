d7798636d733498caa5ddb1019cebbb8fb9857ee
iugamovil@gmail.com <iugamovil@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-06-21 00:27:51 +0000

    Borre el branch de prueba

--- branches/compras/modelo/ISubtotal.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-/**
- * Se implementa a todos las clases del modelo que calculen un subtotal
- * @author Emmanuel
- */
-public interface ISubtotal {
-    public double calcularSubtotal();
-}
