ed441d70aff8a2ee3bbbce0858ae20ab95dd0eae
fermmanuel@gmail.com <fermmanuel@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-08-21 14:34:22 +0000

    Algunos arreglos de la Emisión de Orden de Compra (ISSUE 40) y completando la Recepción de Orden De Compra (ISSUE 48).
    A esto último quiero comentar que ahora, cada vez que presionás "Emitir" te imprime todas las recepciones que posea la orden de compra en cuestión. Habría que hacer un control de "Guarde antes de emitir" y otra ventanita que le avise cuales van a ser las recepciones que estás a punto de imprimir.

--- trunk/vista/reportes/sources/EmitirRecepcionOrdenDeCompra.java
+++ trunk/vista/reportes/sources/EmitirRecepcionOrdenDeCompra.java
@@ -13,20 +13,25 @@ import com.itextpdf.text.pdf.PdfPTable;
 import controlador.users.UserSession;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import modelo.DetalleOrdenDeCompra;
+import modelo.DetalleRecepcionOrdenDeCompra;
 import modelo.OrdenDeCompra;
+import modelo.RecepcionOrdenDeCompra;
 import util.FechaUtil;
 import vista.reportes.ReportDesigner;
 
 /**
  * @author Iuga
  */
-public class EmitirOrdenDeCompra extends ReportDesigner  {
+public class EmitirRecepcionOrdenDeCompra extends ReportDesigner  {
     
     private OrdenDeCompra odc;
+    private RecepcionOrdenDeCompra recepcion;
 
-    public EmitirOrdenDeCompra(OrdenDeCompra odc) {
+    public EmitirRecepcionOrdenDeCompra(OrdenDeCompra odc, RecepcionOrdenDeCompra recepcion) {
         this.odc = odc;
+        this.recepcion = recepcion;
     }
     
     @Override
@@ -53,11 +58,7 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
             Phrase nEPP = new Phrase("\nFecha: ",ReportDesigner.FUENTE_NORMAL_B);
             PTitulo.add(nEPP);  
                 Phrase nEPP2 = new Phrase(String.valueOf(FechaUtil.getFecha(this.odc.getFechaUltimaModificacion())),ReportDesigner.FUENTE_NORMAL);
-                PTitulo.add(nEPP2);                  
-            Phrase nOB = new Phrase("\nForma de Pago: ",ReportDesigner.FUENTE_NORMAL_B);
-            PTitulo.add(nOB); 
-                Phrase nEM2 = new Phrase(String.valueOf(this.odc.getFormaDePago().getNombre()),ReportDesigner.FUENTE_NORMAL);
-                PTitulo.add(nEM2);  
+                PTitulo.add(nEPP2);                    
             Phrase nFI = new Phrase("\nForma de Entrega: ",ReportDesigner.FUENTE_NORMAL_B);
             PTitulo.add(nFI);    
                 Phrase nEM3 = new Phrase(String.valueOf(this.odc.getFormaDeEntrega()),ReportDesigner.FUENTE_NORMAL);
@@ -67,9 +68,9 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
         Paragraph pnl = new Paragraph(new Phrase("\n"));
         super.doc.add(pnl);
         
-        PdfPTable tabla = new PdfPTable(5);
+        PdfPTable tabla = new PdfPTable(6);
         tabla.setWidthPercentage(100);
-        float[] anchos = {1.1f,9.9f,4f,4f,4f};
+        float[] anchos = {1.1f,10f,3f,3f,3f,3f};
         tabla.setWidths(anchos);
         
         Paragraph paNumero = new Paragraph();
@@ -86,15 +87,22 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
         celdaDescripcion.setBackgroundColor(new BaseColor(219,229,241));
         tabla.addCell(celdaDescripcion);
         
+        Paragraph paOrdenado = new Paragraph();
+        paOrdenado.add(new Phrase("Ordenadas", ReportDesigner.FUENTE_NORMAL_B));
+        PdfPCell celdaOrdenado = new PdfPCell(paOrdenado);
+        celdaOrdenado.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        celdaOrdenado.setBackgroundColor(new BaseColor(219,229,241));
+        tabla.addCell(celdaOrdenado);
+        
         Paragraph paCantidad = new Paragraph();
-        paCantidad.add(new Phrase("Cantidad", ReportDesigner.FUENTE_NORMAL_B));
+        paCantidad.add(new Phrase("Recibida", ReportDesigner.FUENTE_NORMAL_B));
         PdfPCell celdaCantidad = new PdfPCell(paCantidad);
         celdaCantidad.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         celdaCantidad.setBackgroundColor(new BaseColor(219,229,241));
         tabla.addCell(celdaCantidad);
         
         Paragraph paPrecioUnitario = new Paragraph();
-        paPrecioUnitario.add(new Phrase("Precio Unit. ", ReportDesigner.FUENTE_NORMAL_B));
+        paPrecioUnitario.add(new Phrase("Precio", ReportDesigner.FUENTE_NORMAL_B));
         PdfPCell celdaPrecioUnitario = new PdfPCell(paPrecioUnitario);
         celdaPrecioUnitario.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         celdaPrecioUnitario.setBackgroundColor(new BaseColor(219,229,241));
@@ -108,14 +116,14 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
         tabla.addCell(celdaSubtotal);
         
         double total = 0;
-        for(int i=0; i< this.odc.getDetalle().size();i++)
+        for(int i=0; i< this.recepcion.getRecepcionesParciales().size();i++)
         {
-            DetalleOrdenDeCompra detalle = this.odc.getDetalle().get(i);
+            DetalleRecepcionOrdenDeCompra detalle = this.recepcion.getRecepcionesParciales().get(i);
             this.pintarDetalle(tabla, detalle, i+1);
-            total += detalle.getCantidad()* detalle.getPrecioUnitario();
+            total += detalle.getCantidad()* detalle.getDetalleOrdenDeCompra().getPrecioUnitario();
         }
         if(total != 0){
-            tabla.addCell("");tabla.addCell("");tabla.addCell("");
+            tabla.addCell("");tabla.addCell("");tabla.addCell("");tabla.addCell("");
             PdfPCell celdaTituloTotal = new PdfPCell(new Paragraph("Total: ",ReportDesigner.FUENTE_NORMAL_B));
             celdaTituloTotal.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
             tabla.addCell(celdaTituloTotal);
@@ -157,7 +165,7 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
         super.doc.add(tablaPie);
     }
 
-    private void pintarDetalle(PdfPTable tabla, DetalleOrdenDeCompra detalle, int index) {
+    private void pintarDetalle(PdfPTable tabla, DetalleRecepcionOrdenDeCompra detalle, int index) {
         Paragraph paNumero = new Paragraph();
         paNumero.add(new Phrase(String.valueOf(index), ReportDesigner.FUENTE_NORMAL));
         PdfPCell celdaNumero = new PdfPCell(paNumero);
@@ -165,29 +173,34 @@ public class EmitirOrdenDeCompra extends ReportDesigner  {
         tabla.addCell(celdaNumero);
         
         Paragraph paDescripcion = new Paragraph();
-        paDescripcion.add(new Phrase(String.valueOf(detalle.getDescripcion()), ReportDesigner.FUENTE_NORMAL));
+        paDescripcion.add(new Phrase(String.valueOf(detalle.getDetalleOrdenDeCompra().getDescripcion()), ReportDesigner.FUENTE_NORMAL));
         PdfPCell celdaDescripcion = new PdfPCell(paDescripcion);
         celdaDescripcion.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         tabla.addCell(celdaDescripcion);
         
-        Paragraph paCantidad = new Paragraph();
-        paCantidad.add(new Phrase(String.valueOf(detalle.getCantidad()), ReportDesigner.FUENTE_NORMAL));
-        PdfPCell celdaCantidad = new PdfPCell(paCantidad);
-        celdaCantidad.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
-        tabla.addCell(celdaCantidad);
+        Paragraph paCantidadOrdenada = new Paragraph();
+        paCantidadOrdenada.add(new Phrase(String.valueOf(detalle.getDetalleOrdenDeCompra().getCantidad()), ReportDesigner.FUENTE_NORMAL));
+        PdfPCell celdaCantidadOrdenada = new PdfPCell(paCantidadOrdenada);
+        celdaCantidadOrdenada.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        tabla.addCell(celdaCantidadOrdenada);
+        
+        Paragraph paCantidadRecibida = new Paragraph();
+        paCantidadRecibida.add(new Phrase(String.valueOf(detalle.getCantidad()), ReportDesigner.FUENTE_NORMAL));
+        PdfPCell celdaCantidadRecibida = new PdfPCell(paCantidadRecibida);
+        celdaCantidadRecibida.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        tabla.addCell(celdaCantidadRecibida);
         
         Paragraph paPrecioUnitario = new Paragraph();
-        paPrecioUnitario.add(new Phrase("$ "+String.valueOf(detalle.getPrecioUnitario()), ReportDesigner.FUENTE_NORMAL));
+        paPrecioUnitario.add(new Phrase("$ "+String.valueOf(detalle.getDetalleOrdenDeCompra().getPrecioUnitario()), ReportDesigner.FUENTE_NORMAL));
         PdfPCell celdaPrecioUnitario = new PdfPCell(paPrecioUnitario);
         celdaPrecioUnitario.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         tabla.addCell(celdaPrecioUnitario);
         
-        double subtotal = detalle.getCantidad()*detalle.getPrecioUnitario();
+        double subtotal = detalle.getCantidad()*detalle.getDetalleOrdenDeCompra().getPrecioUnitario();
         Paragraph paSubtotal = new Paragraph();
         paSubtotal.add(new Phrase("$ "+String.valueOf(subtotal), ReportDesigner.FUENTE_NORMAL));
         PdfPCell celdaSubtotal = new PdfPCell(paSubtotal);
         celdaSubtotal.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         tabla.addCell(celdaSubtotal);
     }
-    
 }
