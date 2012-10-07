653d2168f70a17df7533d6586c45eed58a3d88db
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-10-07 21:08:19 +0000

    - Menu Principal para ejecucion - Arreglado el listado de Planificaciones - Creado el Listado de Ejecuciones

--- controlador/ejecucion/EjecucionUtils.java
+++ controlador/ejecucion/EjecucionUtils.java
@@ -2,38 +2,60 @@ package controlador.ejecucion;
 
 import java.util.ArrayList;
 import modelo.Ejecucion;
+import modelo.PedidoObra;
 import modelo.TareaEjecucion;
-import modelo.TareaPlanificacion;
+import util.HibernateUtil;
 
 /**
  *
  * @author Iuga
  */
 public class EjecucionUtils {
-    
+
     /**
-     * Busca Recursivamente y devuelve Todas las Tareas y SubTareas que haya en una ejecucion
+     * Busca Recursivamente y devuelve Todas las Tareas y SubTareas que haya en
+     * una ejecucion
+     *
      * @param ejecucion
-     * @return 
+     * @return
      */
-    public static ArrayList<TareaEjecucion> getTodasTareasEjecucion(Ejecucion ejecucion)
-    {
+    public static ArrayList<TareaEjecucion> getTodasTareasEjecucion(Ejecucion ejecucion) {
         ArrayList<TareaEjecucion> lista = new ArrayList<TareaEjecucion>();
-        
+
         for (int i = 0; i < ejecucion.getListaTareas().size(); i++) {
             TareaEjecucion tarea = ejecucion.getListaTareas().get(i);
             getTareasRecursivas(lista, tarea);
         }
-        
+
         return lista;
     }
-    private static void getTareasRecursivas(ArrayList<TareaEjecucion> lista, TareaEjecucion tarea)
-    {
+
+    private static void getTareasRecursivas(ArrayList<TareaEjecucion> lista, TareaEjecucion tarea) {
         lista.add(tarea);
         for (int i = 0; i < tarea.getSubtareas().size(); i++) {
             TareaEjecucion tarearec = tarea.getSubtareas().get(i);
             getTareasRecursivas(lista, tarearec);
-        }        
+        }
     }
-    
+
+    /**
+     * Desde un ID de Ejecucion, busco y reetorno el PedidoDeObra
+     *
+     * @param id
+     * @return
+     */
+    public static PedidoObra getNumeroObraFromNumeroEjecucion(int idEjecucion) {
+        try {
+            HibernateUtil.beginTransaction();
+            PedidoObra po = (PedidoObra) HibernateUtil.getSession().createQuery("FROM PedidoObra po WHERE :cID = po.ejecucion.id").setParameter("cID", idEjecucion).uniqueResult();
+            HibernateUtil.commitTransaction();
+            if (po != null) {
+                return po;
+            }
+        } catch (Exception e) {
+            HibernateUtil.rollbackTransaction();
+            return null;
+        }
+        return null;
+    }
 }
---
+++ vista/ejecucion/ConsultarEjecuciones.java
@@ -0,0 +1,65 @@
+package vista.ejecucion;
+
+import controlador.ejecucion.EjecucionUtils;
+import java.util.ArrayList;
+import modelo.Ejecucion;
+import modelo.PedidoObra;
+import util.SwingPanel;
+import vista.gen.PantallaConsultarGenerica;
+
+/**
+ *
+ * @author Iuga
+ */
+public class ConsultarEjecuciones extends PantallaConsultarGenerica{
+
+    public ConsultarEjecuciones(Class entidad) {
+        super(entidad);
+    }
+
+    public ConsultarEjecuciones() {
+        
+        super(Ejecucion.class);
+    }
+    
+    @Override
+    protected ArrayList<String[]> getColumnas()
+    {
+        ArrayList<String[]> columnas = new ArrayList<String[]>();
+        
+            columnas.add(new String[]{"mostrarPedidoObra","Obra"});
+            columnas.add(new String[]{"mostrarFechaInicio","Fecha de Inicio"});
+            columnas.add(new String[]{"mostrarFechaFin","Fecha de Fin"});
+            columnas.add(new String[]{"getEstado","Estado"});
+        
+        return columnas;
+    }
+
+    @Override
+    protected String getNombreVentana() {
+        return "Listado de todas las Ejecuciones";
+    }
+
+    @Override
+    protected ArrayList<String[]> getColumnColorCriteria() {
+        ArrayList<String[]> colorCriteria = new ArrayList<String[]>();
+        
+            colorCriteria.add(new String[]{"Estado",Ejecucion.ESTADO_ALTA,"184","204","240"});
+            colorCriteria.add(new String[]{"Estado",Ejecucion.ESTADO_BAJA,"229","184","183"});
+            colorCriteria.add(new String[]{"Estado",Ejecucion.ESTADO_ENEJECUCION,"255","255","153"});
+            colorCriteria.add(new String[]{"Estado",Ejecucion.ESTADO_FINALIZADA,"204","255","153"});
+            
+        return colorCriteria;
+    }
+
+    @Override
+    protected void abrirEntidad(int id) {
+        PedidoObra pobra = EjecucionUtils.getNumeroObraFromNumeroEjecucion(id);
+        if(pobra!=null){
+            VentanaEjecucion win = new VentanaEjecucion(pobra.getId());
+            SwingPanel.getInstance().addWindow(win);
+            win.setVisible(true);
+        }
+    }
+    
+}
