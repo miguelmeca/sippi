3f7aab60aac2cd1b175313525db64786e86e5e66
fmgs31@gmail.com <fmgs31@gmail.com@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2012-10-28 18:50:44 +0000

    Estados en Ejecucion RRHH

--- trunk/vista/ejecucion/lanzamiento/PanelRecursosHumanos.java
+++ trunk/vista/ejecucion/lanzamiento/PanelRecursosHumanos.java
@@ -59,19 +59,12 @@ public class PanelRecursosHumanos extends javax.swing.JPanel {
 
             },
             new String [] {
-                "Nombre del empleado", "Horas Normales", "Horas 50%", "Horas 100%", "Estado", ""
+                "Nombre del empleado", "Horas Normales", "Horas 50%", "Horas 100%", "Estado"
             }
         ) {
-            Class[] types = new Class [] {
-                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class
-            };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, true
+                false, false, false, false, false
             };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
