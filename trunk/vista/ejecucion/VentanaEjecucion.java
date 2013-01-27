3ddd3c2d99b33ba89926c70107134bbe1105763f
iugamovil <iugamovil@e46bc4c0-00e5-899e-775c-c9a1ab773ae7>
2013-01-27 00:00:15 +0000

    - Fix fechas erroneas en el Gantt
    AGREGAR LA CoolGantt-1.0.5rc.jar y joda-time-2.1.jar a las libs del proyecto.

--- trunk/vista/ejecucion/VentanaEjecucion.java
+++ trunk/vista/ejecucion/VentanaEjecucion.java
@@ -1545,27 +1545,27 @@ public class VentanaEjecucion extends javax.swing.JInternalFrame {
             p1.setEditable(false);
         }
 
-        // WORKARROUND DE HORAS DE TAREAS
-        Calendar fInicio = Calendar.getInstance();
-        fInicio.setTime(tplan.getFechaInicio());
-        fInicio.set(Calendar.HOUR,21);
-        fInicio.set(Calendar.MINUTE,0);
-        fInicio.set(Calendar.SECOND,0);
-        fInicio.set(Calendar.MILLISECOND,0);
-        
-        Calendar fFin = Calendar.getInstance();
-        fFin.setTime(tplan.getFechaFin());
-        fFin.set(Calendar.HOUR,21);
-        fFin.set(Calendar.MINUTE,0);
-        fFin.set(Calendar.SECOND,0);
-        fFin.set(Calendar.MILLISECOND,0);        
+//        // WORKARROUND DE HORAS DE TAREAS
+//        Calendar fInicio = Calendar.getInstance();
+//        fInicio.setTime(tplan.getFechaInicio());
+//        fInicio.set(Calendar.HOUR,21);
+//        fInicio.set(Calendar.MINUTE,0);
+//        fInicio.set(Calendar.SECOND,0);
+//        fInicio.set(Calendar.MILLISECOND,0);
+//        
+//        Calendar fFin = Calendar.getInstance();
+//        fFin.setTime(tplan.getFechaFin());
+//        fFin.set(Calendar.HOUR,21);
+//        fFin.set(Calendar.MINUTE,0);
+//        fFin.set(Calendar.SECOND,0);
+//        fFin.set(Calendar.MILLISECOND,0);        
         
         
         p1.setId(tplan.hashCode());
         p1.setNombre(tplan.getNombre());
         p1.setType(tplan.getEstado());
-        p1.setStartDate(fInicio.getTime());
-        p1.setEndDate(fFin.getTime());
+        p1.setStartDate(tplan.getFechaInicio());
+        p1.setEndDate(tplan.getFechaFin());
 
         // Agrego el nivel de tarea al gantt
         for (int j = 0; j < n; j++) {
